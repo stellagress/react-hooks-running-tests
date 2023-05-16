ba52466486fdd5cd6286da59fe5192f2d286377a
Stella Mazzillo <stellagress5@gmail.com>
2023-05-15 23:09:17 -0400

    complete running tests in react

--- src/__tests__/Article.test.js
+++ src/__tests__/Article.test.js
@@ -7,5 +7,7 @@ import Article from "../components/Article";
 test("displays the text 'please pass this test'", () => {
   render(<Article />);
 
+  
+
   expect(screen.queryByText("please pass this test")).toBeInTheDocument();
 });
